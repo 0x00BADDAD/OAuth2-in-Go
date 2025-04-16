8f3bd1f1ed095b1ae1563bcd08482dd920384b61
0x00BADDAD <tclatharvbhagya@INLTTCL00823.corp.tatacapital.com>
2025-04-16 17:43:53 +0000

    a simple net/http server serving a templated html

---
+++ backend/src/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"hello-OAuth2/src/server"
+)
+
+func main() {
+	server.InitServer() // Listen and serve at port 8080 a simple html template
+}
--- backend/src/server/server.go
+++ backend/src/server/server.go
@@ -4,17 +4,56 @@ package server
 
 import (
 	"fmt"
-	"log"
+	"html/template"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
-func initServer() {
-	h1 := func(w http.ResponseWriter, req *http.Request) {
-		// Access the headers received
-		headersMap := req.Header
-		fmt.Println("Printing the header Map: %v\n", headersMap)
+type tmplData struct {
+	Title string
+	Items []string
+}
+
+func myHandler(w http.ResponseWriter, req *http.Request) {
+	// writes response
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
+	cwd, err := os.Getwd()
+	fmt.Printf("Value of cwd is: %s\n", cwd)
+	if err != nil {
+		fmt.Print("Error in os.Getwd()")
 	}
-	http.HandleFunc("/", h1)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	path_to_file := filepath.Join(cwd, "templates", "landing.tmpl")
+	tmpl, err := template.ParseFiles(path_to_file)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		io.WriteString(w, "Whoops! something went wrong while parsing the template!")
+		fmt.Printf("The error while parsing landing.tmpl is: %v\n", err)
+		return
+	}
+	data := tmplData{
+		Title: ">>Title here!<<",
+		Items: []string{"Sheldon", "Penny", "Raj", "Howard"},
+	}
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		io.WriteString(w, "Whoops! something went wrong when executing the data on template!")
+		fmt.Printf("Err while executing the data on landing.tmpl is %v\n", err)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	return
+}
+
+func InitServer() {
+
+	http.Handle("/", http.HandlerFunc(myHandler))
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Printf("The value of Err when exiting the server: %v\n", err)
+	}
 
 }
